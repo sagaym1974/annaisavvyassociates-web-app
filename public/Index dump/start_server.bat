b60f5a3210806ea411fb180c2a3803e76d0bb22a
Sagay <sagaym1974@gmail.com>
2025-12-06 20:56:24 +0530

    redone with global access to cart in jewellery screen

--- public/Index dump/start_server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start cmd /k "cd /d C:\Users\VIJAY\Desktop\Web Photo Stocks\public && python -m http.server 8000"
-timeout /t 2 /nobreak > nul
-start chrome "http://localhost:8000/index.html"
